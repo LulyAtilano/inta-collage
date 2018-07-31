0c85169cb6ecb0dd67dbac2926186dbb4f5d6806
latilanoh@gmail.com <latilanoh@gmail.com>
2018-07-30 23:41:55 -0500

    se inicia la funcionalidad de login, se realizan pequeños cambios en la estructura y estilo de login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,16 @@
 $(document).ready(function() { 
     $(".dropdown-trigger").dropdown();
 });
+
+let btnLogin = document.getElementById('buttonLogin');
+btnLogin.addEventListener('click', validatePassword);
+
+function validatePassword () {
+    let passwordInput = document.getElementById('password').value;
+    console.log(passwordInput);
+
+
+}
+
+let emaildInput = document.getElementById('email').value;
+console.log(emaildInput);
